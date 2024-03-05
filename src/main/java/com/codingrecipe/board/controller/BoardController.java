908aa705c0bad9fe5c59a21a73c6771a1444e5b9
tbs03 <tbs03112@naver.com>
2024-03-06 03:15:08 +0900

    first commit

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,28 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+            @GetMapping("/save")
+            public String save() {
+                return"save";
+            }
+
+            @PostMapping("/save")
+            public String save(BoardDTO boardDTO) {
+                System.out.println("boardDTO = "+ boardDTO);
+                boardService.save(boardDTO);
+                return "index";
+            }
+
+
+}
---
+++ src/main/java/com/codingrecipe/board/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.codingrecipe.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String index() {
+
+        System.out.println("HELLO");
+        return "index";
+    }
+
+
+
+}
